7674c7c29434fbd8787df99482c86b300b74b09e
Tarun jain <tarun.jain@viithiisys.com>
2023-06-20 17:13:06 +0530

    feat: added typescript package

--- src/components/search-box/seach-input.jsx
+++ src/components/search-box/seach-input.jsx
@@ -1,7 +1,7 @@
 import './search-input.css';
 
 const SearchInput = ({ placeholder , value , onChangeHandler }) => (
-    <input className='search-input' type="search" placeholder={placeholder} onChange={onChangeHandler}/>
+    <input className='search-input' type="search" placeholder={placeholder} onChange={onChangeHandler}  value={value}/>
 );
 
 export default SearchInput;
